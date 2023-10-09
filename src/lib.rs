ef25c948527760e6e88cc35da51929f494e88101
Anubhab Bandyopadhyay <anubhab.bandyopadhyay@gmail.com>
2023-10-09 11:33:00 +0530

    Basic implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,170 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use axum::{
+    http::{header::CONTENT_TYPE, Request},
+    response::{IntoResponse, Response},
+};
+use futures::{future::BoxFuture, ready};
+use http::{HeaderValue, request::Parts, HeaderName, header::{ACCEPT, HOST}};
+use hyper::Method;
+use tower_http::cors::{CorsLayer, AllowOrigin};
+use std::{
+    convert::Infallible,
+    task::{Context, Poll},
+};
+use tower::{Service, layer::util::{Stack, Identity}};
+
+#[macro_use]
+extern crate log;
+
+pub struct MultiplexService<A, B> {
+    rest: A,
+    rest_ready: bool,
+    grpc: B,
+    grpc_ready: bool,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<A, B> MultiplexService<A, B> {
+    pub fn new(rest: A, grpc: B) -> Self {
+        Self {
+            rest,
+            rest_ready: false,
+            grpc,
+            grpc_ready: false,
+        }
     }
+}
+
+impl<A, B> Clone for MultiplexService<A, B>
+where
+    A: Clone,
+    B: Clone,
+{
+    fn clone(&self) -> Self {
+        Self {
+            rest: self.rest.clone(),
+            grpc: self.grpc.clone(),
+            // the cloned services probably wont be ready
+            rest_ready: false,
+            grpc_ready: false,
+        }
+    }
+}
+
+impl<A, B> Service<Request<hyper::Body>> for MultiplexService<A, B>
+where
+    A: Service<Request<hyper::Body>, Error = Infallible>,
+    A::Response: IntoResponse,
+    A::Future: Send + 'static,
+    B: Service<Request<hyper::Body>>,
+    B::Response: IntoResponse,
+    B::Future: Send + 'static,
+{
+    type Response = Response;
+    type Error = B::Error;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        // drive readiness for each inner service and record which is ready
+        loop {
+            match (self.rest_ready, self.grpc_ready) {
+                (true, true) => {
+                    return Ok(()).into();
+                }
+                (false, _) => {
+                    ready!(self.rest.poll_ready(cx)).map_err(|err| match err {})?;
+                    self.rest_ready = true;
+                }
+                (_, false) => {
+                    ready!(self.grpc.poll_ready(cx))?;
+                    self.grpc_ready = true;
+                }
+            }
+        }
+    }
+
+    fn call(&mut self, req: Request<hyper::Body>) -> Self::Future {
+        // require users to call `poll_ready` first, if they don't we're allowed to panic
+        // as per the `tower::Service` contract
+        assert!(
+            self.grpc_ready,
+            "grpc service not ready. Did you forget to call `poll_ready`?"
+        );
+        assert!(
+            self.rest_ready,
+            "rest service not ready. Did you forget to call `poll_ready`?"
+        );
+
+        // if we get a grpc request call the grpc service, otherwise call the rest service
+        // when calling a service it becomes not-ready so we have drive readiness again
+        if is_grpc_request(&req) {
+            self.grpc_ready = false;
+            let future = self.grpc.call(req);
+            Box::pin(async move {
+                let res = future.await?;
+                Ok(res.into_response())
+            })
+        } else {
+            self.rest_ready = false;
+            let future = self.rest.call(req);
+            Box::pin(async move {
+                let res = future.await.map_err(|err| {
+                    error!("Error during json await: {err:?}");
+                    match err {}
+                })?;
+                Ok(res.into_response())
+            })
+        }
+    }
+}
+
+fn is_grpc_request<B>(req: &Request<B>) -> bool {
+    req.headers()
+        .get(CONTENT_TYPE)
+        .map(|content_type| {
+            info!(
+                "{}",
+                String::from_utf8(content_type.as_bytes().to_vec()).unwrap()
+            );
+            content_type.as_bytes()
+        })
+        .filter(|content_type| content_type.starts_with(b"application/grpc"))
+        .is_some()
+}
+
+#[inline]
+fn allow_headers() -> [HeaderName; 17] {
+    [
+        ACCEPT,
+        HOST,
+        CONTENT_TYPE,
+        HeaderName::from_static("x-c"),
+        HeaderName::from_static("x-u"),
+        HeaderName::from_static("x-a"),
+        HeaderName::from_static("x-o"),
+        HeaderName::from_static("x-rsa"),
+        HeaderName::from_static("x-auth"),
+        HeaderName::from_static("x-ua"),
+        HeaderName::from_static("x-host"),
+        HeaderName::from_static("x-key"),
+        HeaderName::from_static("x-app"),
+        HeaderName::from_static("x-rqid"),
+        HeaderName::from_static("x-provider"),
+        HeaderName::from_static("x-grpc-web"),
+        HeaderName::from_static("x-user-agent"),
+    ]
+}
+
+
+pub fn xai_layer() -> Stack<CorsLayer, Identity> {
+    tower::ServiceBuilder::new()
+        .layer(
+            CorsLayer::new()
+                .allow_headers(allow_headers())
+                .allow_origin(AllowOrigin::predicate(|origin: &HeaderValue, _: &Parts| {
+                    origin.is_empty()
+                        || origin.as_bytes().ends_with(b"xambit.io")
+                        || origin.as_bytes().starts_with(b"http://localhost")
+                }))
+                .allow_methods([Method::POST, Method::GET, Method::OPTIONS]),
+        )
+        .into_inner()
 }
